0a16cbb85753a23137dc007f48e08d91cd5c4cb2
hasali <hassan.salim@medlife.com>
2019-01-15 14:49:36 +0530

    Improved apis

--- src/main/java/com/hassan/auth/controlller/AuthController.java
+++ src/main/java/com/hassan/auth/controlller/AuthController.java
@@ -4,52 +4,66 @@ import com.hassan.auth.model.EmailAuth;
 import com.hassan.auth.repository.EmailAuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     @Value("${hashing-salt-text}")
-    private String saltText;
+    private static String saltText;
 
     @Autowired
     private EmailAuthRepository emailAuthRepository;
 
     @PostMapping("/signup")
-    public String createAuthDetails(@RequestBody EmailAuth emailAuth) {
-        String saltedPassword = saltText + ":" + emailAuth.getPassword();
-        String hashedPassword = generateHash(saltedPassword);
-        emailAuth.setPassword(hashedPassword);
-        emailAuthRepository.save(emailAuth);
-        return "Successfully signup user";
+    public HashMap<String, String> createAuthDetails(@RequestBody EmailAuth emailAuth) {
+        try {
+            String hashedPassword = generateHash(emailAuth.getPassword());
+            emailAuth.setPassword(hashedPassword);
+            emailAuthRepository.save(emailAuth);
+        } catch (DataIntegrityViolationException e) {
+            return new HashMap<String, String>() {{
+                put("message", "Existing User");
+            }};
+        }
+        return new HashMap<String, String>() {{
+            put("message", "Successfully signup user");
+        }};
+
     }
 
     @PostMapping("/log")
-    public String logWithEmailAuth(@RequestBody EmailAuth emailAuth) {
+    public HashMap<String, String> logWithEmailAuth(@RequestBody EmailAuth emailAuth) {
         String password = emailAuthRepository.findByEmail(emailAuth.getEmail()).get(0).getPassword();
-        String saltedPassword = saltText + ":" + emailAuth.getPassword();
-        String hashedPassword = generateHash(saltedPassword);
-        System.out.println(hashedPassword);
-        System.out.println(emailAuth.getPassword());
+        String hashedPassword = generateHash(emailAuth.getPassword());
         if(hashedPassword.equals(password))
-            return "Logged in";
+            return new HashMap<String, String>() {{
+                put("message", "Logged in");
+            }};
         else
-            return "Invalid Password";
+            return new HashMap<String, String>() {{
+                put("message", "Invalid Password");
+            }};
     }
 
 
 
     private static String generateHash(String input) {
         StringBuilder hash = new StringBuilder();
+        String saltedInput = saltText + ":" + input;
 
         try {
             MessageDigest sha = MessageDigest.getInstance("SHA-512");
-            byte[] hashedBytes = sha.digest(input.getBytes());
+            byte[] hashedBytes = sha.digest(saltedInput.getBytes());
             char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                     'a', 'b', 'c', 'd', 'e', 'f' };
             for (int idx = 0; idx < hashedBytes.length;   idx++) {
